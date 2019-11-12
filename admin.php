d8c0a269ca50414b7615195b08cb8630dbfbf29a
ijg0341 <51219477+ijg0341@users.noreply.github.com>
2019-11-12 15:46:00 +0900

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ForYou Admin</title>
+    <link href="/static/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+    <input type="text" class="btn btn-primary" value="Hello Bootstrap">
+    ForYou 어드민 페이지입니다.
+</body>
+</html>
